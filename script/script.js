85632e57dab7a5a4a92722cee0d2e27f05e7eaa2
Nick Kuemmling <nick_kuemmling@outlook.com>
2022-07-25 15:12:36 -0400

    first daft

---
+++ script/script.js
@@ -0,0 +1,28 @@
+// HAMBERGER MENU
+const menuButton = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const menuLinks = document.querySelectorAll('.menuLinks');
+const menuIcon = document.querySelector('.fa-solid');
+
+menuButton.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    menuButton.classList.toggle('active');
+    menuIcon.classList.toggle('fa-square-xmark');
+    menuIcon.classList.toggle('fa-bars');
+
+    
+});
+
+menuLinks.forEach((e)=>{
+
+    e.addEventListener('click', ()=>{
+        
+        menu.classList.toggle('active');
+        menuButton.classList.toggle('active');
+        menuIcon.classList.toggle('fa-square-xmark');
+        menuIcon.classList.toggle('fa-bars');
+
+    })
+})
+
+
